b08955e094d5bd896080be9c5969c833096e92cf
vipulchakravarthy <saivipul15@msitprogram.net>
2021-07-19 20:37:23 +0530

    completed the insertion sort

--- ADS/Sorting algos/Solution.java
+++ ADS/Sorting algos/Solution.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 class Solution {
     public static void main(String[] args) {
         int[] arr = { 10, 1, 4, 3, 2, 50, 100000 };
-        SelectionSort obj = new SelectionSort();
+        // SelectionSort obj = new SelectionSort();
+        InsertionSort obj = new InsertionSort();
         obj.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
