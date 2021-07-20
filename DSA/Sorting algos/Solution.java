3dff39f7bf874d23fcdf321a3589bbb5aa948e28
vipulchakravarthy <saivipul15@msitprogram.net>
2021-07-20 17:39:00 +0530

    completed bubble sort

--- DSA/Sorting algos/Solution.java
+++ DSA/Sorting algos/Solution.java
@@ -4,7 +4,8 @@ class Solution {
     public static void main(String[] args) {
         int[] arr = { 10, 1, 4, 3, 2, 50, 100000 };
         // SelectionSort obj = new SelectionSort();
-        InsertionSort obj = new InsertionSort();
+        // InsertionSort obj = new InsertionSort();
+        BubbleSort obj = new BubbleSort();
         obj.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
